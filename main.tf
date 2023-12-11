774ef3f201a626dff8225dfc993022cc7023b007
nicps-git <nicps_@hotmail.com>
2023-12-11 20:57:45 -0300

    Use our Terraform Cloud organization

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
               #!/bin/bash
               apt-get update -y
-              apt-get install -y git wget curl nano vim htop net-tools
+              apt-get install -y git wget curl nano htop net-tools
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
               echo "Hello World" > /var/www/html/index.html
